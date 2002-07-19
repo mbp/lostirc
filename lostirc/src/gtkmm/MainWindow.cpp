10f34946aa83a06783d293f223edef79b8ca89c5
Morten Brix Pedersen <morten@wtf.dk>
2002-07-19 18:40:59 +0000

    Some checks for 0-objects.

--- lostirc/src/gtkmm/MainWindow.cpp
+++ lostirc/src/gtkmm/MainWindow.cpp
@@ -44,11 +44,13 @@ MainWindow::MainWindow()
     add(notebook);
     show_all();
 
-    int num_of_servers = app.start();
-    if (num_of_servers == 0) {
+    if (app.getCfg().getServers().empty()) {
         // Construct initial tab
         Tab *tab = newServer();
         *tab << "\0037\nWelcome to LostIRC "VERSION"!\n\nYou use the client mainly by typing in commands and text in the entry-bar shown below.\n\nYou can connect to a server using:\n    \0038/SERVER <hostname>\n\n\0037Then join a channel:\n    \0038/JOIN <channel-name>\n\n\0037The rest of the commands is available with:\n    \0038/COMMANDS\0037.\n\n\0037Available keybindings:\n    \0038Alt + [1-9] - switch tabs from 1-9.\n    Alt + n - create new server tab.\n    Alt + c - close current tab.\n    Alt + p - open preferences.\n    Tab - nick-completion and command-completion.\n";
+    } else {
+        // Auto-connect to servers.
+        app.start();
     }
 }
 
@@ -71,10 +73,12 @@ void MainWindow::displayMessage(const string& msg, FE::Destination d, bool shoul
     if (d == FE::CURRENT) {
         Tab *tab = notebook.getCurrent();
 
-        *tab << msg;
+        if (tab) {
+            *tab << msg;
 
-        if (shouldHighlight)
-              notebook.highlightActivity(tab);
+            if (shouldHighlight)
+                  notebook.highlightActivity(tab);
+        }
 
     } else if (d == FE::ALL) {
         vector<Tab*> tabs;
@@ -95,10 +99,13 @@ void MainWindow::displayMessage(const string& msg, FE::Destination d, ServerConn
     if (d == FE::CURRENT) {
         Tab *tab = notebook.getCurrent(conn);
 
-        *tab << msg;
+        if (tab) {
+            *tab << msg;
 
-        if (shouldHighlight)
-              notebook.highlightActivity(tab);
+            if (shouldHighlight)
+                  notebook.highlightActivity(tab);
+        }
+
     } else if (d == FE::ALL) {
         vector<Tab*> tabs;
         vector<Tab*>::const_iterator i;
