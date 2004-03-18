b15e3759ff5f74424f363d7c5ec192083a7aaa73
Morten Brix Pedersen <morten@wtf.dk>
2004-03-18 01:35:19 +0000

    2004-03-18  Morten Brix Pedersen  <morten@wtf.dk>
            * Update Copyright to 2004 in all source files.

--- lostirc/src/libirc/ConfigValue.h
+++ lostirc/src/libirc/ConfigValue.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2002, 2003 Morten Brix Pedersen <morten@wtf.dk>
+ * Copyright (C) 2002-2004 Morten Brix Pedersen <morten@wtf.dk>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
