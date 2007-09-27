6207599ed438d1d0a82b45202b35f854beaa81ba
Martin Braure de Callignon <braure@labri.fr>
2007-09-27 20:59:01 +0000

    * Add myself in files I previously modify as Dominik Wagenfuehr suggests me to do.

--- lostirc/src/gtkmm/main.cpp
+++ lostirc/src/gtkmm/main.cpp
@@ -1,5 +1,6 @@
 /* 
  * Copyright (C) 2002-2004 Morten Brix Pedersen <morten@wtf.dk>
+ * Copyright (C) 2007 Martin Braure de Calignon <braurede@free.fr>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
